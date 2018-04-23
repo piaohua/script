2a1adde71840396aafe08f750a05630a1dbf4d6d
piaohua <twopiao7@gmail.com>
2018-04-23 19:07:19 +0800

    update: 添加备份指定集合

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-#mongoexport --port=6002 -d suizhou -c col_user -o col_user.dat
-#mongoimport --port 6002 -d test -c col_robot col_user.dat
+set -e
+
+# mongoexport --port=6002 -d suizhou -c col_user -o col_user.dat
+# mongoimport --port 6002 -d test -c col_robot col_user.dat
+
+# mongoimport -d test -c students --type csv --headerline --file students_csv.dat
+
+# mongodump --collection COLLECTION --db DB_NAME
+# mongorestore -h dbhost -d dbname --directoryperdb dbdirectory
 
 usage() {
     echo " ./ctrl cmd "
     echo " cmd : mongodb (start|stop) "
-    echo " cmd : mongorestore (DBNAME PATH) "
-    echo " cmd : mongo-console, mongodump "
+    echo " cmd : mongo-console "
+    echo " cmd : mongodump 备份整个数据库"
+    echo " cmd : mongorestore (DBNAME PATH) 恢复指定位置备份"
+    echo " cmd : coldump 备份bson格式集合"
+    echo " cmd : colimport (DBNAME PATH) 导入bson格式集合 "
 }
 
 # server conf
@@ -18,6 +28,9 @@ cd $workDir
 # mongodb conf
 db_host=127.0.0.1:27017
 db_name=suizhou
+collections_array=(col_user col_id_gen)
+remote_host=127.0.0.1
+remote_addr=/home/database/backup/
 db_conf=mongodb.conf
 db_path=~/data/mgo
 db_data=${db_path}/data
@@ -26,7 +39,7 @@ db_pidfile=${db_path}/mongodb.pid
 
 MONGODB_DIR="/usr/local/Cellar/mongodb/3.4.4/bin"
 BACKUP_DIR="${db_path}/backup"
-TAR="/bin/tar"
+TAR="/usr/bin/tar"
 
 get_curr_last_month() {
     #echo `date -d last-month +"%Y-%m-%d"`
@@ -41,19 +54,32 @@ get_curr_time() {
     echo `date +"%Y-%m-%d %H:%M:%S"`
 }
 
+get_curr_time_str() {
+    echo `date +"%Y%m%d%H%M%S"`
+}
+
 MONGO="${MONGODB_DIR}/mongo"
 MONGOD="${MONGODB_DIR}/mongod"
+BSONDUMP="${MONGODB_DIR}/bsondump"
 MONGODUMP="${MONGODB_DIR}/mongodump"
 MONGORESTORE="${MONGODB_DIR}/mongorestore"
+MONGOEXPORT="${MONGODB_DIR}/mongoexport"
+MONGOIMPORT="${MONGODB_DIR}/mongoimport"
 LOGFILE="${BACKUP_DIR}/backup_database.log"
 
 CURRDAY=`get_curr_day`
 CURRDAY_LASTMONTH=`get_curr_last_month`
 BACKUPPARENTDIR="${BACKUP_DIR}/${db_name}"
+
 BACKUPDIR="${BACKUPPARENTDIR}/${CURRDAY}"
 BACKUPFILENAME="${BACKUPPARENTDIR}/${db_name}_${CURRDAY}.tar.gz"
+
 BACKUPDIR_LASTMONTH="${BACKUPPARENTDIR}/${CURRDAY_LASTMONTH}"
 BACKUPFILENAME_LASTMONTH="${BACKUPPARENTDIR}/${db_name}_${CURRDAY_LASTMONTH}.tar.gz"
+
+CURR_TIME_STR=`get_curr_time_str`
+BACKUPDIR_STR="${BACKUPPARENTDIR}/${CURR_TIME_STR}"
+BACKUPFILENAME_STR="${BACKUPPARENTDIR}/${db_name}_${CURR_TIME_STR}.tar.gz"
 
 save_log() {
     echo "" >> ${LOGFILE}
@@ -71,7 +97,7 @@ mysql_conn() {
 # processname: mongodb
 mongodb_() {
     test -x $DAEMON || exit 0
-    set -e
+    #set -e
     case "$1" in
         start)
             echo -n "Starting MongoDB... "
@@ -91,6 +117,18 @@ mongodb_() {
             ;;
     esac
     exit 0
+}
+
+remote_backup_sync() {
+    if [ ! -e "${1}" ]
+    then
+        echo "${1} file not exist! "
+        exit 1
+    fi
+    echo "${1} sync to ${remote_host}"
+    save_log "${1} sync to ${remote_host}"
+    scp $1 root@${remote_host}:${remote_addr}
+    save_log "${1} sync done"
 }
 
 mongorestore_() {
@@ -152,6 +190,75 @@ mongodump_() {
     ${TAR} cvf ${BACKUPFILENAME} ${CURRDAY}
 
     save_log "BACKUP DATABASE ${db_name} SUCCEED"
+
+    # remote_backup_sync ${BACKUPFILENAME}
+}
+
+collections_import_by_json() {
+    name=$1
+    if [ "${name}" == "" ]
+    then
+        echo "${$name} db not exist! "
+        exit 1
+    fi
+    path=$2
+    # 目录或文件已经存在，则程序退出
+    if [ ! -d "${path}" ]
+    then
+        echo "${path} directory not exist! "
+        exit 1
+    fi
+
+    for col in ${collections_array[@]};
+    do
+        file=${path}/${col}.json
+        if [ ! -e "${file}" ]
+        then
+            echo "${file} file not exist! "
+            exit 1
+        fi
+        echo "Start import collection ${file} to db ${name}! "
+        save_log "Start import collection ${file} to db ${name}! "
+        ${MONGOIMPORT} -h ${db_host} -d ${name} -c ${col} --type json --file ${file}
+    done
+
+    save_log "RESTORE DATABASE ${name} SUCCEED"
+}
+
+collections_dump_to_json() {
+    # 目录或文件已经存在，则程序退出
+    if [ -d "${BACKUPDIR_STR}" ]
+    then
+        echo "${BACKUPDIR_STR} directory had exist! "
+        exit 1
+    fi
+    if [ -e "${BACKUPFILENAME_STR}" ]
+    then
+        echo "${BACKUPFILENAME_STR} file had exist! "
+        exit 1
+    fi
+
+    mkdir -p ${BACKUPDIR_STR}
+
+    cd ${BACKUPPARENTDIR}
+    for col in ${collections_array[@]};
+    do
+        echo "Start dump collection ${col} to DIR ${BACKUPDIR_STR}"
+        save_log "Start dump collection ${col} to DIR ${BACKUPDIR_STR}"
+        ${MONGODUMP} --host=${db_host} -d ${db_name} --collection ${col} -o ${BACKUPDIR_STR}
+        echo "bson to json ${col} to DIR ${BACKUPDIR_STR}"
+        save_log "bson to json ${col} to DIR ${BACKUPDIR_STR}"
+        ${BSONDUMP} --outFile ${BACKUPDIR_STR}/${db_name}/${col}.json ${BACKUPDIR_STR}/${db_name}/${col}.bson
+    done
+
+    echo "Start compress to ${BACKUPFILENAME_STR}"
+    save_log "Start compress to ${BACKUPFILENAME_STR}"
+    cd ${BACKUPPARENTDIR}
+    ${TAR} cvf ${BACKUPFILENAME_STR} ${CURR_TIME_STR}
+
+    save_log "BACKUP DATABASE ${db_name} SUCCEED"
+
+    # remote_backup_sync ${BACKUPFILENAME_STR}
 }
 
 mongo_console() {
@@ -165,6 +272,10 @@ case $1 in
         mongodump_;;
     mongorestore)
         mongorestore_ $2 $3;;
+    coldump)
+        collections_dump_to_json;;
+    colimport)
+        collections_import_by_json $2 $3;;
     mongo-console)
         mongo_console;;
     mysql)
